d9be3abf6b50237bcc03bccb12304ba9f6d1ba33
Piyush Kumar <piyush.kumar@admitkard.com>
2022-06-19 22:33:15 +0530

    Published jestCoverage

--- dist/index.js
+++ dist/index.js
@@ -195,9 +195,9 @@ const fetchRequiredBranches = () => tslib_1.__awaiter(void 0, void 0, void 0, fu
     // const requiredBranches = [git.base, git.head];
     // echo ${GITHUB_HEAD_REF} ${GITHUB_BASE_REF} ${{ github.event.pull_request.head.sha }}
     yield (0, utils_1.runner)(`git fetch --all`);
-    yield (0, utils_1.runner)(`git fetch --no-tags --depth=0 origin ${git_1.git.base}`);
+    // await runner(`git fetch --no-tags --depth=0 origin ${git.base}`);
     yield (0, utils_1.runner)(`git checkout -b ${git_1.git.base}`);
-    yield (0, utils_1.runner)(`git fetch --no-tags --depth=0 origin ${git_1.git.head}`);
+    // await runner(`git fetch --no-tags --depth=0 origin ${git.head}`);
     yield (0, utils_1.runner)(`git checkout -b ${git_1.git.head}`);
 });
 const getChangedFiles = () => {
